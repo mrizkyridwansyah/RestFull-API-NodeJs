b4b2cb90be97e65b6502e1d13c2ab8fbc47d19bd
mrizkyridwansyah <mrizkyridwansyah29@gmail.com>
2020-09-21 20:24:46 +0700

    add routes to app.js

--- app.js
+++ app.js
@@ -6,4 +6,7 @@ const app = express();
 app.use(bodyparser.urlencoded({extended:true}));
 app.use(bodyparser.json());
 
+let routes = require('./routes');
+routes(app);
+
 app.listen(3000, () => { console.log('running') });
--- controller.js
+++ controller.js
@@ -4,5 +4,9 @@ let response = require('./response.js');
 let connection = require('./connection.js');
 
 exports.index = function (req, res) {
-	response.ok('Aplikasi running');
+	response.ok('Aplikasi running', res);
+}
+
+exports.a = function(req, res) {
+	response.ok('asdf',res);
 }
