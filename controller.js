ff0b09b9ea81fd60ea2b1ffb989ca14b56388875
mrizkyridwansyah <mrizkyridwansyah29@gmail.com>
2020-09-24 12:18:39 +0700

    add login and generate token with jwt

--- controller.js
+++ controller.js
@@ -11,7 +11,7 @@ exports.index = function (req, res) {
 exports.getSupplier = function (req, res) {
 	let sql = "SELECT * FROM ms_supplier";
 	connection.query(sql, function (err, result) {
-		if(err) throw err;
+		if(err) response.message(150, "error !", null, res);
 		if(result.length == 0){
 			response.message(404,"No data for this request",res);
 		}
@@ -24,7 +24,7 @@ exports.getSupplier = function (req, res) {
 exports.getSupplierById = function (req, res) {
 	let sql = "SELECT * FROM ms_supplier WHERE IDSupplier = ? ";
 	connection.query(sql, req.params.id , function (err, result) {
-		if(err) throw err;
+		if(err) response.message(150, "error !", null, res);
 		if(result.length == 0){
 			response.message(404,"No data for this request",res);
 		}
@@ -36,7 +36,7 @@ exports.getSupplierById = function (req, res) {
 
 exports.addSupplier = function (req, res) {
 	connection.query("SELECT RIGHT(MAX(IDSupplier),6) + 1 as jml FROM ms_supplier", function (err,result) {
-		if(err) throw err;
+		if(err) response.message(150, "error !", null, res);
 		let data = {
 			IDSupplier: "SUP." + String("000000" + result[0].jml).slice(-6),
 			NamaSupplier: req.body.nama,
@@ -49,7 +49,7 @@ exports.addSupplier = function (req, res) {
 
 		let sql = "INSERT INTO ms_supplier SET ?";
 		connection.query(sql, data, function (err, result) {
-			if(err) throw err;
+			if(err) response.message(150, "error !", null, res);
 			response.message(200,"Data has been saved", null, res);
 		})
 	});
@@ -57,12 +57,12 @@ exports.addSupplier = function (req, res) {
 
 exports.updateSupplier = function (req, res) {	
 	connection.query("SELECT COUNT(*) as jml FROM ms_supplier WHERE IDSupplier = ?", [req.body.id], function (err,result) {
-		if(err) throw err;
+		if(err) response.message(150, "error !", null, res);
 		if(result[0].jml > 0){
 			let sql = "UPDATE ms_supplier SET NamaSupplier = ?, AlamatSupplier = ?, NoTelp = ?, NoFax = ?, Status = ? WHERE IDSupplier = ?";
 			let data = [req.body.nama, req.body.alamat, req.body.telp, req.body.fax, req.body.status, req.body.id];
 			connection.query(sql, data, function (err, result) {
-				if(err) throw err;
+				if(err) response.message(150, "error !", null, res);
 				response.message(200,"Data has been updated", null, res);
 			})			
 		}
@@ -74,11 +74,11 @@ exports.updateSupplier = function (req, res) {
 
 exports.deleteSupplier = function (req, res) {	
 	connection.query("SELECT COUNT(*) as jml FROM ms_supplier WHERE IDSupplier = ?", [req.body.id], function (err,result) {
-		if(err) throw err;
+		if(err) response.message(150, "error !", null, res);
 		if(result[0].jml > 0){
 			let sql = "DELETE FROM ms_supplier WHERE IDSupplier = ?";
 			connection.query(sql, [req.body.id], function (err, result) {
-				if(err) throw err;
+				if(err) response.message(150, "error !", null, res);
 				response.message(200,"Data has been deleted", null, res);
 			})
 		}
--- middleware/auth.js
+++ middleware/auth.js
@@ -19,16 +19,48 @@ exports.register = function (req, res) {
 
 	let sql = "SELECT email FROM auth_api WHERE email = ?";
 	connection.query(sql, post.email, function (err, result) {
-		if(err) throw err;
+		if(err) response.message(150, "error !", null, res);
 		if(result.length > 0){
 			response.message(409, "The email already exists", null, res);
 		}
 		else{
 			sql = "INSERT INTO auth_api SET ?";
 			connection.query(sql, post, function (err, result) {
-				if(err) throw err;
+				if(err) response.message(150, "error !", null, res);
 				response.ok(200, "Register success!", null , res);
 			})
+		}
+	})
+}
+
+exports.login = function (req, res) {
+	let sql = "SELECT * FROM auth_api WHERE email = ? AND password = ?";
+	connection.query(sql, [req.body.email, md5(req.body.password)], function (err, result) {
+		if(err) response.message(150, "error !", null, res);
+		if(result.length > 0){
+			let token = jwt.sign({result}, config.secret, {
+				expiresIn: 1440//25 menit
+			});
+
+			let data = {
+				id_user: result[0].id,
+				access_token: token,
+				ip_address: ip.address()
+			}
+			console.log(data);
+			sql = "INSERT INTO akses_token SET ?";
+			connection.query(sql, data, function (err, result) {
+				if(err) response.message(150, "error !", null, res);
+				let dataResponse = {
+					Token: token,
+					IdUser: data.id_user
+				};
+
+				response.message(200, "Token has been generated", dataResponse, res);
+			})
+		}
+		else{
+			response.message(401, "Invalid email or password", null, res);
 		}
 	})
 }
